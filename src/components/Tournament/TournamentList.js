95d9f2061a9e80d4b5e208b004d5867b557891f0
Facundo Flores <flores.facundogabriel@exa.unsa.edu.ar>
2017-09-23 09:45:17 -0300

    Add SmartTable component Add tournamentlist using smarttable

--- src/components/Tournament/TournamentList.js
+++ src/components/Tournament/TournamentList.js
@@ -1,5 +1,53 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { SmartTable } from '../../shared'
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name"
+  },
+  {
+    title: "Start Date",
+    dataIndex: "start_date",
+    key: "start_date"
+  },
+  {
+    title: "Amount Teams",
+    dataIndex: "amount_teams",
+    key: "amount_teams"
+  },
+  {
+    title: "Description",
+    dataIndex: "description",
+    key: "description"
+  }
+];
+
+const tournamentsList = [
+  {
+    "id": "1",
+    "name": "sit et",
+    "description": "Accusamus dolores ea accusantium repudiandae. In exercitationem dolorum et aut a eos.",
+    "start_date": "Tue Jul 25 1995 00:00:00 GMT+0000 (UTC)",
+    "amount_teams": 16
+  },
+  {
+    "id": "2",
+    "name": "dignissimos in",
+    "description": "Non consequatur quis aspernatur nulla. Ratione consectetur sunt magni et magnam voluptas rerum.",
+    "start_date": "Wed Jun 11 1975 00:00:00 GMT+0000 (UTC)",
+    "amount_teams": 17
+  },
+  {
+    "id": "3",
+    "name": "est dicta",
+    "description": "Et ut sit nemo. Aliquam dignissimos vel dolorum accusantium porro qui minima hic id.",
+    "start_date": "Wed Sep 16 1998 00:00:00 GMT+0000 (UTC)",
+    "amount_teams": 19
+  }
+]
 
 export default class TournamentList extends Component {
   static propTypes = {
@@ -8,7 +56,11 @@ export default class TournamentList extends Component {
 
   render() {
     return (
-      <h1>Tournaments List</h1>
+      <SmartTable
+        dataSource={tournamentsList}
+        columns={columns}
+        rowKey="id"
+      />
     )
   }
 }
---
+++ src/shared/SmartTable/index.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import { PropTypes } from "prop-types";
+import { Button, Table } from "antd";
+
+export const SHOW_RECORD = "show";
+export const EDIT_RECORD = "edit";
+export const REMOVE_RECORD = "remove";
+
+const ACTIONS_OBJ = emit => ({
+  title: "Action",
+  dataIndex: "",
+  key: "x",
+  render: (text, record, index) =>
+    <span>
+      <Button
+        icon="search"
+        onClick={() => emit(text, record, index, SHOW_RECORD)}
+      />
+      <Button
+        icon="edit"
+        onClick={() => emit(text, record, index, EDIT_RECORD)}
+      />
+      <Button
+        icon="delete"
+        onClick={() => emit(text, record, index, REMOVE_RECORD)}
+      />
+    </span>
+});
+
+class SmartTable extends Component {
+  constructor(props) {
+    super(props);
+    const { columns, dataSource, emit, pagination, rowKey } = this.props;
+    this.state = {
+      columns: columns.concat(ACTIONS_OBJ(emit)),
+      dataSource,
+      rowKey,
+      pagination
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      dataSource: nextProps.dataSource,
+      pagination: nextProps.pagination
+    });
+  }
+
+  handleTableChange = (pagination, filters, sorter) => {
+    this.props.handleTableChange(pagination, filters, sorter);
+  };
+
+  render() {
+    const { columns, dataSource, pagination, rowKey } = this.state;
+    return (
+      <Table
+        dataSource={dataSource}
+        pagination={pagination}
+        columns={columns}
+        rowKey={rowKey}
+        onChange={this.handleTableChange}
+      />
+    );
+  }
+}
+
+SmartTable.defaultProps = {
+  rowKey: ""
+};
+
+SmartTable.propTypes = {
+  dataSource: PropTypes.array.isRequired,
+  pagination: PropTypes.object.isRequired,
+  handleTableChange: PropTypes.func.isRequired,
+  emit: PropTypes.func.isRequired,
+  rowKey: PropTypes.string
+};
+
+export default SmartTable;
