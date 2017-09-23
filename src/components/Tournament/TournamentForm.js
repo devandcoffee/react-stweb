241ac02a9d4b44674ffdf287409344512e4f62ad
Facundo Flores <flores.facundogabriel@exa.unsa.edu.ar>
2017-09-23 20:38:30 -0300

    Add queries for getting tourney info

--- src/components/Tournament/TournamentForm.js
+++ src/components/Tournament/TournamentForm.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+const TITLE_NEW = 'New Tournament';
+const TITLE_EDIT = 'Editing Tournament';
+
 export default class TournamentForm extends Component {
   static propTypes = {
     id: PropTypes.string,
+    data: PropTypes.object,
   }
 
   static defaultProps = {
     id: '',
+    data: {},
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: props.id ? TITLE_EDIT : TITLE_NEW,
+      activeRecord: null,
+    };
+  }
+
+  componentWillReceiveProps = (nextProps) => {
+    if (nextProps.data.tourney) {
+      const { title } = this.state;
+      this.setState({
+        activeRecord: nextProps.data.tourney,
+        title: `${title} ${nextProps.data.tourney.name}`,
+      });
+    }
   }
 
   render() {
-    const { id } = this.props;
+    const { title } = this.state;
     return (
       <div>
-        {
-          !id && <h1> New Tournament </h1>
-        }
-        {
-          id && <h1> Editing the tournament: {id} </h1>
-        }
+        <h1> {title} </h1>
       </div>
     )
   }
--- src/containers/TournamentFormContainer.js
+++ src/containers/TournamentFormContainer.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { gql, graphql } from 'react-apollo';
 import { TournamentForm } from '../components';
+
+const getTournament = gql`query getTournament($id: Int!) {
+  tourney(id: $id) {
+    id
+    name
+    amount_teams
+    start_date
+    amount_teams
+    description
+    user {
+      id
+    }
+    tourney_type {
+      id
+      name
+    }
+  }
+}`;
+
+const TournamentFormWithData = graphql(getTournament, {
+  options: ({ id }) => ({ variables: { id: id ? id : 0 } }),
+})(TournamentForm);
 
 function TournamentFormContainer({ match }) {
   return (
-    <TournamentForm id={match.params.id} />
+    <TournamentFormWithData id={match.params.id} />
   )
 }
+
+TournamentFormContainer.propTypes = {
+  match: PropTypes.object.isRequired,
+}
+
 export default TournamentFormContainer;
